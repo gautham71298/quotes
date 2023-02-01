c59f964ba1ac8e1b274b11f409da00856f2e383b
Gautham <gowthamkumar71298@gmail.com>
2023-02-01 17:28:56 +0530

    Initial Commit

---
+++ Quotes App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quotes App
+//
+//  Created by Gautham on 01/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
